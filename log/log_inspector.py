b5aba38724a89256d68da943ed3d843c46c2d498
home <jasuil@daum.net>
2023-12-10 18:36:56 +0900

    로그분석툴 추가

---
+++ log/log_inspector.py
@@ -0,0 +1,92 @@
+import json
+import os
+from datetime import datetime
+import sys
+
+"""
+log파일을 텍스트모드로 분석합니다.
+"""
+
+# 로그파일에 있는 key들을 여기에 넣어서 내용을 찾도록 합니다.
+searchKeys = ['uri', 'response', 'statusCode']
+
+
+# 로그의 구조체를 선언합니다.
+class Log:
+    def __init__(self, data):
+        info = data.split("- access-log: ")
+        self.createTime = datetime.strptime(info[0], '%Y-%m-%d %H:%M:%S.%f')
+        info = json.loads(info[1])
+        self.request = info["request"]
+        self.response = info["response"]
+        self.user = info["user"]
+
+    def __str__(self):
+        return "createTime :: " + str(self.createTime) + "\n" + "request :: " + str(
+            self.request) + "\n" + "response :: " + str(self.response) + "\n" + "user :: " + str(self.user)
+
+
+def load(filename):
+    lines = []
+    loadingText = str()
+    starPoint = 1
+	
+    with open(filename, "r", encoding="utf-8") as f:
+        while True:
+            loadingText = "Loading{}".format("." * starPoint)
+            sys.stdout.write("\r" + loadingText)
+
+            line = f.readline()
+            if not line:
+                break
+            log = Log(line)
+            lines.append(log)
+
+            if starPoint > 10:
+                starPoint = 0
+            starPoint += 1
+            sys.stdout.flush()
+
+    print(" " * len(loadingText), end="\r")
+    return lines
+
+
+def process(data):
+    while True:
+
+        for idx in range(len(searchKeys)):
+            print(str(idx + 1) + '.' + searchKeys[idx], end=', ')
+        print(str(len(searchKeys) + 1) + '.exit', end=' ')
+        selection = int(input(":: what do you next?"))
+
+        if selection == len(searchKeys) + 1:
+            break
+        else:
+            selection -= 1
+            keyword = input("what is the keyword?")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            findIndex = []
+            for i in range(len(data)):
+                if searchKeys[selection] in data[i].request and keyword in str(
+                        data[i].request[searchKeys[selection]]) or \
+                        searchKeys[selection] in data[i].response and keyword in str(data[i].response[
+                                                                                         searchKeys[selection]]) or \
+                        data[i].user is not None and searchKeys[selection] in data[i].user and keyword in str(
+                    data[i].user[
+                        searchKeys[selection]]):
+                    findIndex.append(i)
+
+        if len(findIndex) > 0:
+            print("****** found! ******")
+            count = 0
+            for idx in findIndex:
+                count += 1
+                print(str(count))
+                print(data[idx].__str__())
+                print("********************")
+
+
+if __name__ == "__main__":
+    lines = load(input("input file name: "))
+    print(":: load is finished ::")
+    process(lines)
